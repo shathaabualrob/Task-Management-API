4314e911349b1d6bdc6fa4bdca21baa95a26ed48
shatharub5@gmail.com <shatharub5@gmail.com>
2024-06-02 17:40:09 +0300

    Update request express

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,11 +1,13 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
+import { User } from "../entities/User";
+import { getRepository } from "typeorm";
 
 require('dotenv').config();
 const jwtSecretKey = process.env.JWT_SECRET_KEY;
 
 
-export const authMiddleware = (req: Request, res: Response, next:  NextFunction) => {
+export const authMiddleware = async (req: Request, res: Response, next:  NextFunction) => {
     const token = req.headers.authorization?.split(" ")[1];
     if(!token){
         return res.status(401).json({ message: "No token provided" });
@@ -16,7 +18,14 @@ export const authMiddleware = (req: Request, res: Response, next:  NextFunction)
             throw new Error('JWT_SECRET_KEY is not defined');
         }
         const decoded = jwt.verify(token, jwtSecretKey) as { userId: number };
-        req.user = decoded;
+        const userRepository = getRepository(User);
+        const user = await userRepository.findOne({where: {id: decoded.userId}});
+
+        if (!user) {
+        return res.status(401).json({ message: "User not found" });
+        }
+
+        req.user = user;
         next();
     }catch(error){
         return res.status(401).json({ message: "Invalid token" });
--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,7 +1,8 @@
 import { Request } from "express";
+import { User } from "../entities/User";
 
 declare module "express-serve-static-core" {
   interface Request {
-    user?: { userId: number };
+    user?: User;
   }
 }
